2448624e603dc18133ccae3b4c1e9a692f313f24
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-21 09:56:36 +0200

    ADD corrections

--- js/palindroma.js
+++ js/palindroma.js
@@ -1,15 +1,15 @@
 let word = prompt('Inserisci una parola')
-word=word.toLowerCase();
 console.log(isPalindrom(word));
 
 function isPalindrom(word){
+    word = word.toLowerCase();
     let reversed = '';
     for(let i=0; i<word.length;i++)
     {
         let char = word.at(-i-1);
         reversed += char
     }
-    if (word==reversed){
+    if (word === reversed){
         return true
     }
     else {
